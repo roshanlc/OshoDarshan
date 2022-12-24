f76741bb0dfd28dedaf6d84c83061d96232485c8
Roshan Lamichhane <lamichhanersn@gmail.com>
2022-12-24 12:10:17 +0545

    Some changes to strings.xml

--- app/src/main/java/com/yantra/helloworld/MainActivity.java
+++ app/src/main/java/com/yantra/helloworld/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("INFO", "onCreate: Hello there, born anew.");
+        Log.d("INFO", "onCreate: Osho's timeless teachings.");
     }
 }
